f0c6d7744c6c6dbb3d1d4790e970ea0594db439b
mcdesign <mindcastdesign@gmail.com>
2017-02-23 07:28:18 +0200

    initial commit

---
+++ app/src/main/java/pixeldotme/florinflorescu/ffl/com/pixeldotme/PixelDotMe.java
@@ -0,0 +1,13 @@
+package pixeldotme.florinflorescu.ffl.com.pixeldotme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PixelDotMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pixel_dot_me);
+    }
+}

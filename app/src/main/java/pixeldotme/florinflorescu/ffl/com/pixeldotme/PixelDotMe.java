307bb775ea23bd10f2cb0edba0e2f189ce196709
florinflorescu <florin.florescu@gmail.com>
2017-02-27 07:50:34 +0200

    Adding background service Adding Http class - draft

--- app/src/main/java/pixeldotme/florinflorescu/ffl/com/pixeldotme/PixelDotMe.java
+++ app/src/main/java/pixeldotme/florinflorescu/ffl/com/pixeldotme/PixelDotMe.java
@@ -1,13 +1,36 @@
 package pixeldotme.florinflorescu.ffl.com.pixeldotme;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 //test
 
 
-
 public class PixelDotMe extends AppCompatActivity {
+    final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 0x12;
+    final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 0x13;
+    final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0x14;
+
+    boolean bHaveAllPermissions = true;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +38,34 @@ public class PixelDotMe extends AppCompatActivity {
         setContentView(R.layout.activity_pixel_dot_me);
 
 
-        Intent intent = new Intent(this, PixelDotMeService.class);
-        startService(intent);
+        //ask for permissions to track the device
 
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            bHaveAllPermissions = false;
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            bHaveAllPermissions = false;
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+        }
+
+
+        if (bHaveAllPermissions) {
+            Intent intent = new Intent(this, PixelDotMeService.class);
+            startService(intent);
+        } else {
+            Toast.makeText(this,"Please allow the app to use your location and restart", Toast.LENGTH_LONG).show();
+        }
         /*
            Log.i("MainActivity", "onCreate");
 
@@ -28,5 +76,83 @@ public class PixelDotMe extends AppCompatActivity {
 
 
          */
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("PixelDotMe Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+/*
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "BLE is not supported", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter mBluetoothAdapter = bluetoothManager.getAdapter();
+
+*/
+
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        Log.i("MENU","Menu option "+item.getItemId());
+        if (item.getItemId() == R.id.id_main_menu_settings)
+        {
+            Log.i("MENU - ","item is settings");
+            Intent i = new Intent("android.intent.action.MAIN").putExtra("some_msg", "I will be sent!");
+            i.putExtra("cocotier","baga mare");
+            this.sendBroadcast(i);
+
+        }
+
+        return true;
     }
 }
--- app/src/main/java/pixeldotme/florinflorescu/ffl/com/pixeldotme/PixelDotMeService.java
+++ app/src/main/java/pixeldotme/florinflorescu/ffl/com/pixeldotme/PixelDotMeService.java
@@ -1,19 +1,26 @@
 package pixeldotme.florinflorescu.ffl.com.pixeldotme;
 
+import android.Manifest;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
+import android.os.Process;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.widget.Toast;
-import android.os.Process;
 
 /**
  * Created by florin.florescu on 2/24/2017.
@@ -22,16 +29,16 @@ import android.os.Process;
 public class PixelDotMeService extends Service implements LocationListener {
 
 
-
-
     private Looper mServiceLooper;
     private ServiceHandler mServiceHandler;
+    private BroadcastReceiver mReceiver;
 
     // Handler that receives messages from the thread
     private final class ServiceHandler extends Handler {
         public ServiceHandler(Looper looper) {
             super(looper);
         }
+
         @Override
         public void handleMessage(Message msg) {
             // Normally we would do some work here, like download a file.
@@ -50,7 +57,6 @@ public class PixelDotMeService extends Service implements LocationListener {
     }
 
 
-
     @Override
     public void onCreate() {
         // Start up the thread running the service.  Note that we create a
@@ -64,6 +70,27 @@ public class PixelDotMeService extends Service implements LocationListener {
         // Get the HandlerThread's Looper and use it for our Handler
         mServiceLooper = thread.getLooper();
         mServiceHandler = new ServiceHandler(mServiceLooper);
+
+
+
+        IntentFilter intentFilter = new IntentFilter(
+                "android.intent.action.MAIN");
+
+        mReceiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //extract our message from intent
+                String msg_for_me = intent.getStringExtra("some_msg");
+                //log our message value
+                Log.i("InchooTutorial", msg_for_me);
+                String cocotier = intent.getStringExtra("cocotier");
+                Log.i("InchooTutorial", cocotier);
+
+            }
+        };
+        //registering our receiver
+        this.registerReceiver(mReceiver, intentFilter);
     }
 
     @Override
@@ -77,6 +104,60 @@ public class PixelDotMeService extends Service implements LocationListener {
         mServiceHandler.sendMessage(msg);
         Log.i("Service", "onCreate");
         // If we get killed, after returning from here, restart
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                //Your logic that service will perform will be placed here
+                //In this example we are just looping and waits for 1000 milliseconds in each loop.
+                while (true) {
+                    try {
+                        Thread.sleep(5000);
+                        Log.i("Mainloop", "Recording is .");
+                    } catch (Exception e) {
+                    }
+/*
+                    if(isRunning){
+                        //Log.i(TAG, "Service running");
+                        if (recording_to_start) {
+                            arec = new HubbleAudioRecord();
+                            arec.start();
+                            recording_started = true;
+                            recording_to_start = false;
+                        }
+                        if (recording_to_stop) {
+                            arec.my_stop();
+                            recording_to_stop = false;
+                            recording_started = false;
+
+                        }
+                    }
+                    */
+                }
+
+                //Stop service once it finishes its task
+                //stopSelf();
+            }
+        }).start();
+
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+           // return TODO;
+        }
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        mlocManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, this);
         return START_STICKY;
     }
 
@@ -100,7 +181,7 @@ public class PixelDotMeService extends Service implements LocationListener {
 
     @Override
     public void onLocationChanged(Location location) {
-
+        Log.i("LOCATION","loc lat"+location.getLatitude()+" lon:"+location.getLongitude());
     }
 
     @Override
@@ -117,6 +198,7 @@ public class PixelDotMeService extends Service implements LocationListener {
     public void onProviderDisabled(String provider) {
 
     }
+
 
 
 

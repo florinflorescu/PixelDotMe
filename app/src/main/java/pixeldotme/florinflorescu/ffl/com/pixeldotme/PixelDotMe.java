7deb6ccbb5c4f110f0322adf49d97e666f90d2d1
florinflorescu <florin.florescu@gmail.com>
2017-04-16 12:55:10 +0300

    Adding BLE scanning

--- app/src/main/java/pixeldotme/florinflorescu/ffl/com/pixeldotme/PixelDotMe.java
+++ app/src/main/java/pixeldotme/florinflorescu/ffl/com/pixeldotme/PixelDotMe.java
@@ -2,22 +2,47 @@ package pixeldotme.florinflorescu.ffl.com.pixeldotme;
 
 import android.Manifest;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.net.ParseException;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
 //test
 
 
@@ -25,14 +50,50 @@ public class PixelDotMe extends AppCompatActivity {
     final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 0x12;
     final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 0x13;
     final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0x14;
-
+    TextView textStatus, textIntValue, textStrValue;
     private BroadcastReceiver mReceiver;
     boolean bHaveAllPermissions = true;
+    Messenger mService = null;
+    boolean mIsBound;
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+
+
+
+
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+
+
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case PixelDotMeService.MSG_SET_INT_VALUE:
+                    textIntValue.setText("Int Message: " + msg.arg1);
+                    break;
+                case PixelDotMeService.MSG_SET_STRING_VALUE:
+                    String str1 = msg.getData().getString("str1");
+                    textStrValue.setText("Str Message: " + str1);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        Log.d("Activity", "onDestroy: ");
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -135,9 +196,185 @@ public class PixelDotMe extends AppCompatActivity {
         {
             Log.i("MENU - ","item is settings");
 
-
+            if (PixelDotMeService.isRunning()) {
+                doBindService();
+            }
         }
 
         return true;
+    }
+
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mService = new Messenger(service);
+            //textStatus.setText("Attached.");
+            try {
+                Message msg = Message.obtain(null, PixelDotMeService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+            }
+            catch (RemoteException e) {
+                // In this case the service has crashed before we could even do anything with it
+            }
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been unexpectedly disconnected - process crashed.
+            mService = null;
+            //textStatus.setText("Disconnected.");
+        }
+    };
+
+    void doBindService() {
+        bindService(new Intent(this, PixelDotMeService.class), mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+        //textStatus.setText("Binding.");
+    }
+    void doUnbindService() {
+        if (mIsBound) {
+            // If we have received the service, and hence registered with it, then now is the time to unregister.
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null, PixelDotMeService.MSG_UNREGISTER_CLIENT);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                }
+                catch (RemoteException e) {
+                    // There is nothing special we need to do if the service has crashed.
+                }
+            }
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+            //textStatus.setText("Unbinding.");
+        }
+    }
+
+
+    private int PostUuidLocationImei(String sUuid, String sLat, String sLong, String sImei, String url)
+    {
+        Map<String,Object> params = new LinkedHashMap();
+        TelephonyManager telephonyManager = null ;
+
+        telephonyManager = (TelephonyManager)getApplication().getSystemService(Context.TELEPHONY_SERVICE);
+        //API 18
+        //params.put("register_imei", telephonyManager.getDeviceId(1));
+        params.put("imei", telephonyManager.getDeviceId());
+        params.put("uuid", sUuid);
+        params.put("Lat", sLat);
+        params.put("Long",sLong);
+
+        try {
+            makeHttpPost(params,url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    return 0;
+    }
+
+    private int makeHttpPost(Map<String,Object> params, String my_url) throws IOException {
+        URL url = new URL(my_url);
+
+
+
+
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String,Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
+        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+        String cstr;
+        cstr="";
+        for (int c; (c = in.read()) >= 0;) {
+            System.out.print((char) c);
+            cstr += (char)c;
+        }
+        Log.i("HTTP:",cstr);
+
+
+        Object obj = null;
+        JSONParser parser = new JSONParser();
+        try {
+            try {
+                obj = parser.parse(cstr);
+            } catch (org.json.simple.parser.ParseException e) {
+                e.printStackTrace();
+            }
+
+            JSONArray jsonArray = (JSONArray) obj;
+
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+
+                JSONObject jsonObjectRow = (JSONObject) jsonArray.get(i);
+
+                String nume_atribut = (String)jsonObjectRow.get("att_name");
+                String val_atribut = (String) jsonObjectRow.get("att_value");
+                Log.i("JSON:",nume_atribut+":"+val_atribut);
+  /*
+                if (nume_atribut.equals("AlarmPhone")) {
+                    cfgRet.alarm_phone = val_atribut;
+                }
+                if (nume_atribut.equals("AlarmPhone_1")) {
+                    cfgRet.alarm_phone_1 = val_atribut;
+                }
+                if (nume_atribut.equals("AlarmPhone_2")) {
+                    cfgRet.alarm_phone_2 = val_atribut;
+                }
+                if (nume_atribut.equals("AlarmPhone_3")) {
+                    cfgRet.alarm_phone_3 = val_atribut;
+                }
+                if (nume_atribut.equals("AlarmPhone_4")) {
+                    cfgRet.alarm_phone_4 = val_atribut;
+                }
+                if (nume_atribut.equals("zone_1")) {
+                    cfgRet.zone_1 = val_atribut;
+                }
+                if (nume_atribut.equals("zone_2")) {
+                    cfgRet.zone_2 = val_atribut;
+                }
+                if (nume_atribut.equals("zone_3")) {
+                    cfgRet.zone_3 = val_atribut;
+                }
+                if (nume_atribut.equals("zone_4")) {
+                    cfgRet.zone_4 = val_atribut;
+                }
+
+*/
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+
+        return conn.getResponseCode();
     }
 }
--- app/src/main/java/pixeldotme/florinflorescu/ffl/com/pixeldotme/PixelDotMeService.java
+++ app/src/main/java/pixeldotme/florinflorescu/ffl/com/pixeldotme/PixelDotMeService.java
@@ -2,6 +2,14 @@ package pixeldotme.florinflorescu.ffl.com.pixeldotme;
 
 import android.Manifest;
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,6 +17,8 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,11 +26,16 @@ import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
+import android.os.Messenger;
+import android.os.ParcelUuid;
 import android.os.Process;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by florin.florescu on 2/24/2017.
@@ -33,9 +48,25 @@ public class PixelDotMeService extends Service implements LocationListener {
     private ServiceHandler mServiceHandler;
     private BroadcastReceiver mReceiver;
 
+    static final int MSG_REGISTER_CLIENT = 1;
+    static final int MSG_UNREGISTER_CLIENT = 2;
+    static final int MSG_SET_INT_VALUE = 3;
+    static final int MSG_SET_STRING_VALUE = 4;
 
+    public static final ParcelUuid MY_SERVICE = ParcelUuid.fromString("00006810-864C-02B7-E5CA-9EA81A6EEFE0");
+
+    private static boolean isRunning = false;
+    private int counter = 0, incrementby = 1;
+    ArrayList<Messenger> mClients = new ArrayList<Messenger>(); // Keeps track of all current registered clients.
+
+    final Messenger mMessenger = new Messenger(new IncomingHandler()); // Target we publish for clients to send messages to IncomingHandler.
 
     private final IBinder mBinder = new LocalBinder();
+
+    private static final String TAG = "BeaconActivity";
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeScanner mBluetoothLeScanner;
+
 
     /**
      * Class for clients to access.  Because we know this service always
@@ -86,11 +117,129 @@ public class PixelDotMeService extends Service implements LocationListener {
         // Get the HandlerThread's Looper and use it for our Handler
         mServiceLooper = thread.getLooper();
         mServiceHandler = new ServiceHandler(mServiceLooper);
+        isRunning = true;
+
+
+        BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+
+        mBluetoothAdapter = manager.getAdapter();
+        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+
+
+        ScanFilter beaconFilter = new ScanFilter.Builder()
+                .setServiceUuid(MY_SERVICE)
+                .build();
+
+        ParcelUuid  myuuid = beaconFilter.getServiceUuid();
+
+        ArrayList<ScanFilter> filters = new ArrayList<ScanFilter>();
+        //filters.add(beaconFilter);
+
+
+
+        ScanSettings settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .build();
+
+        mBluetoothLeScanner.startScan(filters, settings, mScanCallback);
+
+
 
 
 
     }
 
+    private ScanCallback mScanCallback = new ScanCallback() {
+        public void onScanResult(int callbackType, ScanResult result) {
+            // Log.d(TAG, "onScanResult "+result.toString());
+            //Toast.makeText(getApplicationContext(),"callback",Toast.LENGTH_SHORT).show();
+
+            byte[] bali = result.getScanRecord().getBytes();
+
+
+
+
+
+            StringBuilder sb = new StringBuilder();
+
+
+            StringBuilder str_mfg = new StringBuilder();
+            str_mfg.append(String.format("%02X", bali[6]));
+            str_mfg.append(String.format("%02X", bali[5]));
+
+            StringBuilder str_prefix = new StringBuilder();
+            str_prefix.append(String.format("%02X", bali[8]));
+            str_prefix.append(String.format("%02X", bali[7]));
+
+
+
+
+
+/*
+                for (byte b : bali) {
+                    sb.append(String.format("%02X ", b));
+                }
+*/
+            for (int i=0;i<bali.length;i++)
+            {
+                sb.append(String.format("%02X ", bali[i]));
+            }
+
+            Log.d("Filtered UUID is", sb.toString());
+
+            //  if ((bali[5]==0x4c) && (bali[6]==0x0))
+            {
+                StringBuilder str_tx_power = new StringBuilder();
+                str_tx_power.append(String.format("%d", bali[29]));
+                double f_m = 0;//calculateAccuracy(bali[29],result.getRssi());
+
+//                if (f_m < 0)
+                //                   return;
+                Log.i("---","----------------------------------------");
+                //Log.i("Beacon Mfg:", str_mfg.toString());
+                //Log.i("Beacon Pfx:", str_prefix.toString());
+                //Log.i("BeaconRSSi:",String.format("%d",result.getRssi()));
+                //Log.i("BeaconName:",String.format("%d",result.getScanRecord().getTxPowerLevel()));
+                ParcelUuid s=null;
+
+                if (result.getScanRecord() != null)
+                if (result.getScanRecord().getServiceUuids()!=null)
+                    s = result.getScanRecord().getServiceUuids().get(0);
+                Log.i("Beacon UUID: ",s.toString());
+                Log.i("Beacon TX: ", (String.format("%x", result.getScanRecord().getTxPowerLevel())));
+                Log.i("Beacon RSSI: ", (String.format("%d", result.getRssi())));
+                if (result.getDevice() != null && result.getDevice().getName()!=null)
+                    Log.i("Beacon Nm :",result.getDevice().getName());
+                //Log.i("zero - ", ""+result.getScanRecord().getManufacturerSpecificData()[0]);
+                BluetoothDevice btDevice = result.getDevice();
+                btDevice.fetchUuidsWithSdp();
+                ParcelUuid[] retval = btDevice.getUuids();
+                //Log.d(TAG, "onScanResult "+result.toString());
+                //Log.i("btDevice:", btDevice.toString());
+                //result.getScanRecord().getManufacturerSpecificData().toString();
+
+
+
+
+
+
+            }
+        }
+
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            Log.d(TAG, "onBatchScanResults: "+results.size()+" results");
+            for (ScanResult result : results) {
+                // processResult(result);
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.w(TAG, "LE Scan Failed: "+errorCode);
+        }
+    };
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
@@ -115,6 +264,8 @@ public class PixelDotMeService extends Service implements LocationListener {
                     try {
                         Thread.sleep(5000);
                         Log.i("Mainloop", "Recording is .");
+                        ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
+                        toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
                     } catch (Exception e) {
                     }
 /*
@@ -175,7 +326,28 @@ public class PixelDotMeService extends Service implements LocationListener {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+
+        return mMessenger.getBinder();
+
+    }
+
+    class IncomingHandler extends Handler { // Handler of incoming messages from clients.
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_REGISTER_CLIENT:
+                    mClients.add(msg.replyTo);
+                    break;
+                case MSG_UNREGISTER_CLIENT:
+                    mClients.remove(msg.replyTo);
+                    break;
+                case MSG_SET_INT_VALUE:
+                    incrementby = msg.arg1;
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
     }
 
     @Override
@@ -199,6 +371,9 @@ public class PixelDotMeService extends Service implements LocationListener {
     }
 
 
-
+    public static boolean isRunning()
+    {
+        return isRunning;
+    }
 
 }
